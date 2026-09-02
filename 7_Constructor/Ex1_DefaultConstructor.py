46f10274cf6668e54f61c85697bb521fc164b798
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-09-02 08:09:04 +0530

    2nd Sept 2026

---
+++ 7_Constructor/Ex1_DefaultConstructor.py
@@ -0,0 +1,35 @@
+print("-----Ex1: Default Constructor------")
+
+
+class Demo1:
+
+    #default Constructor
+    #use1: copy all the members of class into object
+    # def __init__(self):
+    #     constructor body
+
+    def m1(self):
+        print("Running non-static method from Demo1 class")
+
+    def m2(self):
+        print("Running non-static method from Demo1 class")
+
+
+d1=Demo1()        #Object Creation -> constructor calling
+d1.m1()
+
+
+# 1: d1 -> objectName   -> use to refer/identify an object
+#2: Demo1()  -> className()  -> constructor calling  -> copy all the members of class into object
+
+
+
+class Demo2:
+
+    @staticmethod
+    def m2():
+        print("running static method")
+
+Demo2.m2()
+
+
---
+++ 7_Constructor/Ex2_UserDefinedConstructor_WithoutParameter.py
@@ -0,0 +1,48 @@
+print("-----Ex1.1: user defined without parameter constructor------")
+
+class Demo2:
+
+    #user defined constructor
+    #use1: copy all the members of class into object
+    def __init__(self):
+        print("running user defined constructor")
+
+    def m3(self):
+        print("method m3 from Demo2 class")
+
+    def m4(self):
+        print("method m4 from Demo2 class")
+
+d2=Demo2()     #constructor calling
+d2.m3()
+d2.m4()
+
+
+
+print("---")
+
+
+class Demo3:
+
+    #user defined without parameter
+    #use1: copy all the members of class into object
+    #use2: initialize instance variable
+    def __init__(self):
+        self.num1=10            # instance/object variable
+        self.num2=20
+
+    def add(self):
+        print(self.num1+self.num2)
+
+    def mult(self):
+        print(self.num1 * self.num2)
+
+
+d3=Demo3()
+d3.add()
+d3.mult()
+
+
+
+
+
