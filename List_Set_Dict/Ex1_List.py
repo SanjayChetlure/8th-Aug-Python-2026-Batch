876eedca2f17c00ab9c1119a36fa8a9a499200a5
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-09-22 08:03:31 +0530

    22nd sept 2026

--- List_Set_Dict/Ex1_List.py
+++ List_Set_Dict/Ex1_List.py
@@ -15,6 +15,13 @@ print(len(s1))
 print(s1[0])      #amol
 # print(s1[5])    #IndexError: list index out of range
 
+#update data
+s1[0]="AMOL"
+print(s1)
+
+#check specific element present or not
+print(s1.__contains__("A"))
+
 
 #Add data in list append/insert/extend
 #Add new element at last position
@@ -41,6 +48,59 @@ print(s1)
 #remove specific element -> left shift operation
 s1.remove("mahesh")
 print(s1)
+
+#copy list object
+s2=s1.copy()
+print(s2)
+
+print("--print all data using for loop---")
+#             1<8
+for i in range(0,8):
+    print(s1[i])      #s1[0]
+
+print("----")
+
+#             1<8
+for i in range(0,len(s1)):
+    print(s1[i])      #s1[0]
+
+
+print("--print all data using for each loop---")
+for i in s1:
+    print(i)
+
+
+
+#Sorting operation
+s3=[20,50,10,40,30]
+print("------Before sorting-------")
+print(s3)
+
+print("-----After sorting-> Ascending order------")
+s3.sort()       #[10,20,30,40,50]
+print(s3)
+
+
+print("-----sort data in reverse order (descending order)------")
+s3.reverse()     #[50,40,30,20,10]
+print(s3)
+
+
+print("----Count occurrence of specific element in list object------")
+s4=[20,50,10,40,30,50,10,50]
+print(s4.count(15))
+
+
+print("---delete all data from list object---")
+s4.clear()
+print(s4)
+
+
+print("-----delete list object-----")
+del s4
+# print(s4)
+
+
 
 
 
---
+++ List_Set_Dict/Ex2_Set.py
@@ -0,0 +1,9 @@
+
+s1={"amol",101,65.5,"A+",101,101}
+
+print(s1)
+
+print(len(s1))
+print(type(s1))
+
+print(s1.__contains__(102))
